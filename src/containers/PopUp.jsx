2d3b2ae77122267d1be6a1d39952b9c4ab9d2a9e
Andres Prieto <andresprieto2703@gmail.com>
2022-11-13 03:46:05 -0500

    Popup
    Adding popup component

--- src/containers/PopUp.jsx
+++ src/containers/PopUp.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const PopUp = ({ label, children }) => {
+import Close from "../assets/icons/close.svg";
+
+const PopUp = ({ popupData, handler }) => {
   return (
-    <div className="PopUp">
-      <h2>{label}</h2>
-      {children}
-    </div>
+    <section className="popup">
+      <article className="box row popup--header">
+        <h2 className="popup--header__title">
+          {popupData.title} <span>{popupData.target}</span>
+        </h2>
+        <figure className="icon popup--header__icon" onClick={handler}>
+          <img src={Close} alt="Close" srcSet={Close} className="icon" />
+        </figure>
+      </article>
+      <div className="box popup--content">{popupData.content}</div>
+    </section>
   );
 };
 
